6ccc392b19685349b0edb2503388568f35f8aeb9
jalmaguer <jalmague@gmail.com>
2015-11-10 23:19:52 -0500

    update word dump script to include the learning_words table

--- dump_words.py
+++ dump_words.py
@@ -2,14 +2,22 @@ import sqlite3
 import csv
 
 """
-Script to dump words from known_words table to .csv
+Script to dump words from known_words and learning_words tables to .csv
 """
 
 conn = sqlite3.connect('reader_app.db')
 cur = conn.cursor()
-cur.execute('SELECT word FROM known_words')
 
+cur.execute('SELECT word FROM known_words')
 with open('data/known_words.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     rows = cur.fetchall()
     writer.writerows(rows)
+
+cur.execute('SELECT word FROM learning_words')
+with open('data/learning_words.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    rows = cur.fetchall()
+    writer.writerows(rows)
+
+conn.close()
