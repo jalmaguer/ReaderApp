78031f83fc65271353716718acbb9ceda1156361
jalmaguer <jalmague@gmail.com>
2021-04-01 15:50:39 -0400

    fix so that all words on page change status when clicked instead of only one

--- reader_app/static/text_controller.js
+++ reader_app/static/text_controller.js
@@ -2,20 +2,25 @@ let spans = document.body.getElementsByTagName("span");
 let languageID = parseInt(document.getElementById("languageIDHiddenContent").textContent);
 for (let span of spans) {
     if (span.className == "unknown" || span.className == "known") {
-        span.addEventListener("click", event => {
-            let oldClass = event.target.className;
-            let word = event.target.textContent;
-            let newClass;
-            if (oldClass == "unknown") {
-                newClass = "known";
-                event.target.className = newClass;
-            } else if (oldClass == "known") {
-                newClass = "unknown";
-                event.target.className = newClass;
-            }
-            postData({"word": word, "removeFrom": oldClass, "addTo": newClass, "languageID": languageID});
-        });
+        span.addEventListener("click", clickFunction);
     }
+}
+
+function clickFunction(event) {
+    let oldClass = event.target.className;
+    let word = event.target.textContent.toLowerCase();
+    let newClass;
+    if (oldClass == "unknown") {
+        newClass = "known";
+    } else if (oldClass == "known") {
+        newClass = "unknown";
+    }
+    for (let span of spans) {
+        if (span.textContent.toLocaleLowerCase() == word) {
+            span.className = newClass;
+        }
+    }
+    postData({"word": word, "removeFrom": oldClass, "addTo": newClass, "languageID": languageID});
 }
 
 function postData(data) {
